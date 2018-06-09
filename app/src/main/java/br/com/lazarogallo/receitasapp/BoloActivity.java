f081e7e4acad6eb05c7ab1be93662b51803b5d3f
wlamirl <wlamirl@yahoo.com.br>
2018-06-08 21:41:13 -0300

    Primeiro commit

---
+++ app/src/main/java/br/com/lazarogallo/receitasapp/BoloActivity.java
@@ -0,0 +1,13 @@
+package br.com.lazarogallo.receitasapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BoloActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bolo);
+    }
+}
